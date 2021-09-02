567e8857bd3652937cd8fdc4b25f00423482d582
FT31743849 <guillermo.calderaro@softtek.com>
2021-09-02 10:52:09 -0300

    reemplazo de api localidades

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -28,27 +28,8 @@ const Home = () => {
   }, [])
 
    useEffect(() => {
-
-    const municipios = ["Avellaneda", "Lanus", "Lomas de Zamora", "Almirante Brown"]
-
-    if (localidades.localidades) {
-      let mappedLocs = []   //todas
-      let locs = []         //array final
-      mappedLocs = localidades.localidades.map(l => (
-        {
-          id: l.id,
-          nombre: l.nombre,
-          municipio: l.municipio.nombre
-        }
-      ))
-
-      municipios.forEach(m => {
-        let locsPorMuni = mappedLocs.filter(l => l.municipio === m)
-        locs = [...locs, ...locsPorMuni]
-      })
-      setLocalidades(locs)
+      console.log(localidades)
       setCargando(false)
-    }
   }, [localidades])
 
   useEffect(() => {
