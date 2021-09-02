eedc4c391a814749a6c0e8ba4b0ce295f97b085f
Matias Velasquez <asap_mvelasquez@lnexternos.com.ar>
2021-09-02 17:20:46 -0300

    grid

--- src/components/CardLocal.jsx
+++ src/components/CardLocal.jsx
@@ -1,30 +1,54 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Estilos } from '../style/estilos';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Estilos } from "../style/estilos";
 
-const useStyles = makeStyles((theme) => Estilos(theme))
+const useStyles = makeStyles((theme) => Estilos(theme));
 
 export const CardLocal = (props) => {
   //Estilos
   const classes = useStyles();
 
   //parametros
-  const {
-    nombreLocal,
-    telefono,
-    urlLogo
-  } = props
+  const { nombreLocal, telefono, urlLogo } = props;
 
   return (
-    <div class={classes.cardHorizontal}>
-      <img class={classes.imgLocal} src={urlLogo} alt="test"></img>
-      <div class={classes.textoCardLocal}>
-        <div class='titulo-local'>{nombreLocal}</div>
-        <div class='entrega-local'> Entrega entre 20 y 30 minutos</div>
-        <div class='telefono'>Teléfono: {telefono}</div>
-      </div>
+    <div className={classes.cardHorizontal}>
+      <Paper className={classes.paper}>
+        <Grid container spacing={2}>
+          <Grid item>
+            <ButtonBase className={classes.image}>
+              <img className={classes.img} alt="logo" src={urlLogo} />
+            </ButtonBase>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography
+                  gutterBottom
+                  variant="subtitle1"
+                  className={classes.tituloLocal}
+                >
+                  {nombreLocal}
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  Entrega entre 20 y 30 minutos
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  <PhoneIcon fontSize="small" />
+                  Teléfono: {telefono}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Typography variant="subtitle1">
+                4.1 <StarIcon />
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
     </div>
   );
 };
 
-export default CardLocal
+export default CardLocal;
