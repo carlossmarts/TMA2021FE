7c965b439cd0192998c11797570542be681062c8
nit-jrodriguez <jrodriguez@nahualit.com>
2021-10-13 23:07:42 -0300

    agregar comentario

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -7,6 +7,7 @@ export const Carrito = (props) => {
     const shippingPrice = 89
     const totalPrice = itemsPrice + shippingPrice;
     const [direccion, setDireccion] = useState('');
+    const [comentario, setComentario] = useState('');
     const [mensaje, setMensaje] = useState('');
 
     useEffect(() => {
@@ -15,10 +16,12 @@ export const Carrito = (props) => {
         cartItems.map((item) => (
             text = text.concat(`${item.nombre} (${item.qty} x $${item.precio.toFixed(2)}) \n `)
         ))
+        if (comentario !== "")
+            text = text.concat(`*Comentarios:* ${comentario} \n`)
         text = text.concat(`*Total:* $${totalPrice}. \n Mi dirección es: ${direccion}. Gracias`)
 
         setMensaje(`https://wa.me/549${telefono}?text=${encodeURI(text)}`)
-    }, [cartItems, direccion])
+    }, [cartItems, direccion, comentario])
 
     return (
         <Box height="75%" width='35%' px={2}>
@@ -65,12 +68,24 @@ export const Carrito = (props) => {
                                             m={50}
                                             id="outlined-multiline-static"
                                             multiline
-                                            rows={4}
+                                            rows={2}
                                             fullWidth
                                             label="Ingrese su dirección"
                                             variant="outlined"
                                             value={direccion}
                                             onChange={e => setDireccion(e.target.value)} />
+                                    </Box>
+                                    <Box my={1}>
+                                        <TextField
+                                            m={50}
+                                            id="outlined-multiline-static"
+                                            multiline
+                                            rows={2}
+                                            fullWidth
+                                            label="¿Algún comentario sobre tu pedido? ¡Hacelo acá!"
+                                            variant="outlined"
+                                            value={comentario}
+                                            onChange={e => setComentario(e.target.value)} />
                                     </Box>
                                     <Box >
                                         <Button variant="contained" size="small" onClick={() => {
